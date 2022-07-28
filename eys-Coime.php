e6ad4b74bd14a7a861480fe25a2032aee7af9636
jcoime8 <jcoime8@gmail.com>
2022-07-28 15:28:38 +0000

    actualisacion

---
+++ eys-Coime.php
@@ -0,0 +1,93 @@
+
+<?php
+include("head.php");
+?>
+
+
+<?php
+include("marco-right.php");
+?>
+   
+ <div style="float:left; width:60%; margin:60px auto; padding:1vw">
+   <div  style="width: 100%;">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
+        	<h1>¿Quién es Damaris Miranda?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por:  Coime John <br>
+                Fecha: 2021-08-16 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">Soy Coime Bone John Jayron, tengo 19 a?os y soy de la ciudad de Esmeraldas, actualmente realiso mis estudios superiores en la universidad tecnica Luis Vargas Torres. Provengo de una familia humilde donde me inculcaron valores tales como la honestidad, responsabilidad y la solidaridad. </p>
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo">Mis estudios secundarios fueron realisados en el colegio "Margarita Cortes".</p>
+
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático Soy futuro ingeniero en sistemas informaticos y estoy inmersa en el ambito del desarrollo web. </h2> 
+    
+
+
+                   <p  class="eys-parrafo">Durante mi carrera universitaria he logrado manejar herramientas como HTML, CSS, Javascript y en el manejo de linux de forma basica.</p>
+
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis pasatiempos son la lectura, la musica, la creaccion de juegos y dormir :v.</h2> 
+    
+
+                   <p   class="eys-parrafo">En mis ratos libres prefiero jugar algun juego para despejar mi mente.</p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
+
+
+
+
+<?php
+include("marco-left.php")
+?>
+ 
+<script >  
+
+ document.title="Softskill"; 
+document.getElementById("foto").src="./images/John.jpg";
+
+ document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
+
+document.getElementById("elnombre").innerHTML="Coime John";
+
+
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-stalin.php'>¿Quién es Stalin Francis?</a></p><p><a href='eys-kevin.php'>¿Quién es Kevin Aguilar?</a></p> ";
+
+ </script>
+ 
+<?php
+include("foot.php")
+?>
