218eb9682de912fbc35c9be2633cf0f2c66b8498
Janner Guagua <estiven.guagua1@gmail.com>
2022-07-28 12:04:04 -0500

    Se ah realizado la actualizacion de el archivo eys-janner.php

--- eys-janner.php
+++ eys-janner.php
@@ -7,51 +7,49 @@ include("head.php");
 <?php
 include("marco-right.php");
 ?>
-   
+
  <div style="float:left; width:60%; margin:60px auto; padding:1vw">
-   <div  style="width: 100%;">     
+   <div  style="width: 100%;">
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left;">
-        	<h1>¿Quién es Kevin Aguilar?</h1>
-       		 <hr>
-             
+                <h1>¿Quién es Damaris Miranda?</h1>
+                 <hr>
+
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  janner guagua<br>
-                Fecha: 2022-07-27 <br>
+                Posteado por:  Janner Guagua<br>
+                Fecha: 2022-07-28 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Soy Kevin Aguilar, tengo 22 años de edad, nací en la ciudad de Esmeraldas, actualmente soy estudiante de la Universidad Técnica Luis Vargas Torres, estudio la carrera de Ingenieria en Tecnologias de la Información. </p>
-                   <p class="eys-parrafo">Siempre me llamo la atención los temas que traten sobre informatica, pese a que la tecnologia va avanzando, cada dia hay algo nuevo que aprender.</p>
-
+                   <p class="eys-parrafo"> Mis nombres completos son Janner Steeven Guagua Alcivar, actualmente tengo 20 años de edad, vivo en la ciudad de esmeraldas en el Barrio 20 de Noviembre, vivo con mis abuelos por parte de padre desde que tenia 3 Meses de nacido, desde muy pequeño me han llamdao la atencion los sistemas operativos, las paguinas web y todo lo que se pueda programar. </p>
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
-    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
-    
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2>
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Maria Auxiliadora, donde seguí la carrera de informatica, se podría decir que fue en esta etapa donde me intereso aprender más sobre el diseño y creación de páginas web.</p>
-                   <p class="eys-parrafo">Aprendi a manejar lenguaje de etiquetas en 6to curso y aprendí algunas bases sobre creación de paginas web, para la creación de aplicaciones de escritorio utilizaba el lenguaje de programacion java.</p>
+
+                   <p class="eys-parrafo"> Mis estudios secundarios los realice en la Unidad Educativa "5 de Agosto", en el cual me gradue como Soporte y Servicio Tecnico. </p>
+
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
-    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
-    
+    <h2 style="border-bottom: 1px solid green;"> Actualmente estoy en el 5to ciclo academico en la Universidad Tecnica Luis Vargas Torres de Esmeraldas, en esta institucion estoy aprendiendo y adquiriendo una multitud de conocimientos para demostrarlo en un futuro cuando me gradue de Ingeniero en Sistemas Informaticos.  </h2>
 
 
-                   <p  class="eys-parrafo">Durante mi carrera universitaria he logrado manejar herramientas como c++, VisualStudio, Netbeans y he trabajado con diferentes entornos como Atom, VisualStudio Code, sublime text y otros.</p>
-                   <p  class="eys-parrafo">Me gusta mucho aprender sobre temas informaticos, crear paginas web y aprender cada dia algo nuevo sobre la programación, me gustaria aprender a manejar herramientas como phyton y php.</p>
+
+                   <p  class="eys-parrafo"> En este coto tiempo que llevo en el desarrolo de mi carrera eh aprendido difernetes lenguajes de programacion, actualmente estoy familiarizando con el entorno de desarrollo Lunux. </p>
+
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
-    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
-    
+    <h2 style="border-bottom: 1px solid green;"> ¿Que adtividades realizas en tu tiempo libre?</h2>
 
-                   <p   class="eys-parrafo">En mis ratos libres me gusta practicar muchas cosas productivas, como entonar la guitarra, hacer deporte, y siempre trato de aprender cosas nuevas viendo videos tutoriales.</p>
-       
 
-                   
+                   <p   class="eys-parrafo"> En mis ratos libres los utilizo para investigar, escuhar y ver multiples cosas sobre los sistemas operativos, paguinas web, lenguajes de programacion y como utilizarlos. </p>
+
+
+
 
                <hr>
                <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
@@ -62,10 +60,9 @@ include("marco-right.php");
                   <div style="width: 40%; float: right; text-align: right;">
                    <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
                   </div>
-
-                </div>
+</div>
            </div>
-  </div> 
+  </div>
 
   </div>
 
@@ -75,19 +72,21 @@ include("marco-right.php");
 <?php
 include("marco-left.php")
 ?>
- 
-<script >  
 
- document.title="Softskill"; 
-document.getElementById("foto").src="./images/kevin.jpg";
+<script >
+
+ document.title="SoyJanner";
+document.getElementById("foto").src="./images/estiven.jpg";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
+document.getElementById("elnombre").innerHTML="Janner Guagua";
 
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-stalin.php'>¿Quién es Stalin Francis?</a></p><p><a href='eys-damaris.php'>¿Quién es Damaris Miranda?</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-stalin.php'>¿Quién es Stalin Francis?</a></p><p><a href='eys-kevin.php'>¿Quién es Kevin Aguilar?</a></p><p><a herf='eys-Janner.php'>¿Quien es Janner Guagua? ";
+
  </script>
- 
+
 <?php
 include("foot.php")
 ?>
